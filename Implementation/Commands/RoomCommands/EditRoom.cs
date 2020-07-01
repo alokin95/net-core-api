eb9df10a930ca30d60c77cc4f973b10688360591
alokin95 <nikolamitrovic1406@gmail.com>
2020-07-01 20:28:54 +0200

    amenities update

--- Implementation/Commands/RoomCommands/EditRoom.cs
+++ Implementation/Commands/RoomCommands/EditRoom.cs
@@ -3,10 +3,13 @@ using Application.DataTransfer;
 using Application.Exceptions;
 using AutoMapper;
 using DataAccess;
+using Domain.Entity;
 using FluentValidation;
 using Implementation.Validations;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Implementation.Commands.RoomCommands
@@ -31,11 +34,26 @@ namespace Implementation.Commands.RoomCommands
         {
             this.editRoomValidation.ValidateAndThrow(dto);
 
-            var room = this.context.Rooms.Find(dto.Id);
+            var room = this.context.Rooms
+                .Include(r => r.Amenities)
+                .FirstOrDefault(r => r.Id == dto.Id);
 
             if (room == null)
             {
                 throw new EntityNotFoundException(dto.Id);
+            }
+
+            room.Amenities.Where(amenities => amenities.RoomId == room.Id)
+                .ToList()
+                .ForEach(amenity => room.Amenities.Remove(amenity));
+
+            foreach (var amenity in dto.Amenities)
+            {
+                room.Amenities.Add(new RoomAmenity
+                {
+                    AmenityId = amenity.AmenityId,
+                    RoomId = room.Id
+                });
             }
 
             this.mapper.Map(dto, room);
